2bc67589821589b6b30354cd73c10002e8c79a26
Wargoz <lawyer.varganov@gmail.com>
2022-10-23 00:03:46 +0500

    add outlets

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -9,5 +9,35 @@ import UIKit
 
 class ProfileView: UIView {
     
+    @IBOutlet weak var nameImage: UIImageView!
     
+    @IBOutlet weak var nameLabel1: UILabel!
+    
+    @IBOutlet weak var nameLabel2: UILabel!
+    
+    @IBOutlet weak var nameLabel3: UILabel!
+    
+    @IBOutlet weak var nameTextView: UITextView!
+    
+    
+    override init(frame: CGRect) {
+        super.init (frame: frame)
+        self.commonInit ()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init (coder: coder)
+        self.commonInit ()
+    }
+    
+    private func commonInit () {
+        
+        let bundle = Bundle.init (for: ProfileView.self)
+        if let viewToAdd = bundle.loadNibNamed("ProfileView", owner: self, options: nil), let contentView = viewToAdd.first as? UIView {
+            addSubview(contentView)
+            contentView.frame = self.bounds
+        }
+        
+    }
 }
+
