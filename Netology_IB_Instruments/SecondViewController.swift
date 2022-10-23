9823a720de0decffcc7a2127c610f15b386a8c61
Wargoz <lawyer.varganov@gmail.com>
2022-10-23 18:44:47 +0500

    add new storyboard and xib install on this storyboard

---
+++ Netology_IB_Instruments/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Александр Варганов on 23.10.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+}
--- Netology_IB_Instruments/ViewController.swift
+++ Netology_IB_Instruments/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileView: ProfileView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    private func loadProfileView () {
+        
     }
 
 
